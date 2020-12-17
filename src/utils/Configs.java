8b83abfa8821656aaf9b4da68ca15b59e3e2c807
uyentt99 <51995172+uyentt99@users.noreply.github.com>
2020-12-17 15:57:25 +0700

    Xem thong tin xe dang thue

--- src/utils/Configs.java
+++ src/utils/Configs.java
@@ -47,6 +47,7 @@ public class Configs {
 	public static final String THUEXE_CHONTHE_PATH  = "/views/fxml/chonTheThanhToan.fxml";
 	public static final String THUEXE_XACNHAN_PATH  = "/views/fxml/xacNhanThanhToan.fxml";
 	public static final String THONGTIN_BAIXE_PATH  = "/views/fxml/xacNhanThanhToan.fxml";
+	public static final String TRAXE_THONGTINXE_PATH  = "/views/fxml/traxeThongTinXe.fxml";
 	public static Font REGULAR_FONT = Font.font("Segoe UI", FontWeight.NORMAL, FontPosture.REGULAR, 24);
 
 	public static String[] PROVINCES = { "Bắc Giang", "Bắc Kạn", "Cao Bằng", "Hà Giang", "Lạng Sơn", "Phú Thọ",
